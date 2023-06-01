1fa4334eca667d7565857acce849804fba4fa23e
“Matt <mattcowgill@gmail.com>
2023-06-01 09:09:28 +0800

    export geom_richlabel

--- R/geom_richlegend.R
+++ R/geom_richlegend.R
@@ -1,3 +1,34 @@
+#' Rich legend
+#' @description `geom_richlegend()` draws coloured text in lieu of a legend
+#' @inheritParams ggplot2::geom_text
+#' @export
+#' @examples
+#' palmerpenguins::penguins |>
+#'   ggplot(aes(x = bill_length_mm,
+#'              y = bill_depth_mm,
+#'              col = species)) +
+#'   geom_point() +
+#'   geom_richlegend(aes(label = species),
+#'                   x = 60,
+#'                   y = 12.75,
+#'                   vjust = 0,
+#'                   hjust = 1) +
+#'   theme(legend.position = "none")
+#'
+#' # respects facets!
+#' palmerpenguins::penguins |>
+#'   ggplot(aes(x = bill_length_mm,
+#'              y = bill_depth_mm,
+#'              col = species)) +
+#'   geom_point() +
+#'   geom_richlegend(aes(label = species),
+#'                   x = 60,
+#'                   y = 12.75,
+#'                   vjust = 0,
+#'                   hjust = 1) +
+#'   facet_wrap(~island) +
+#'   theme(legend.position = "none")
+
 geom_richlegend <-
   function(mapping = NULL,
            data = NULL,
@@ -7,7 +38,6 @@ geom_richlegend <-
            show.legend = FALSE,
            inherit.aes = TRUE,
            ...) {
-
     ggplot2::layer(
       data = data,
       mapping = mapping,
@@ -18,7 +48,6 @@ geom_richlegend <-
       inherit.aes = inherit.aes,
       params = list(
         na.rm = na.rm,
-
         ...
       )
     )
@@ -29,63 +58,48 @@ geom_richlegend <-
 GeomRichLegend <- ggplot2::ggproto(
   "GeomRichLegend",
   ggplot2::Geom,
-  extra_params = c("richlegend_x", "richlegend_y", "na.rm"),
+  extra_params = c("na.rm"),
   setup_data = function(data, params) {
-    # print(data)
-    x <- ggtext::GeomRichText$setup_data(data, params) |>
+    ggtext::GeomRichText$setup_data(data, params) |>
       dplyr::group_by(label, group, PANEL, colour) |>
       dplyr::summarise() |>
-      dplyr::mutate(x = params$richlegend_x,
-                    y = params$richlegend_y) |>
       dplyr::ungroup()
-
-    # print(x)
-    x
   },
   draw_panel = function(data,
                         panel_params,
                         coord,
                         flipped_aes = FALSE) {
-
     richtext_data <- data |>
       dplyr::mutate(
-        label = paste0("<span style='color:",
-                           colour,
-                           "'>",
-                           label,
-                           "</span>")
+        label = paste0(
+          "<span style='color:",
+          colour,
+          "'>",
+          label,
+          "</span>"
+        )
       ) |>
-      dplyr::group_by(PANEL, x, y, size, angle, hjust, vjust,
-                      alpha, lineheight, family, fontface) |>
+      dplyr::group_by(
+        PANEL, x, y, size, angle, hjust, vjust,
+        alpha, lineheight, family, fontface
+      ) |>
       dplyr::summarise(label = paste0(label, collapse = "<br>")) |>
       dplyr::ungroup() |>
       as.data.frame() |>
       dplyr::mutate(colour = "#000000")
 
-    print(richtext_data)
-
-    panel_params_env <<- panel_params
-    data_env <<- richtext_data
-
-    # ggtext::GeomRichText$draw_panel(
-    #   data,
-    #   panel_params,
-    #   coord
-    # )
     rt_draw_panel(
       richtext_data,
       panel_params,
       coord
     )
-
-    # ggplot2::GeomText$draw_panel(
-    #   richtext_data,
-    #   panel_params,
-    #   coord
-    # )
   },
   draw_key = ggplot2::draw_key_text,
-  required_aes = c("x", "y", "label"),
+  required_aes = c(
+    "x",
+    "y",
+    "label"
+  ),
   default_aes = ggplot2::aes(
     colour = ggplot2::GeomText$default_aes$colour,
     size = ggplot2::GeomText$default_aes$size,
@@ -119,20 +133,20 @@ rt_draw_panel <- function(data, panel_params, coord,
     data$hjust <- compute_just(data$hjust, data$x)
   }
   gridtext::richtext_grob(data$label, data$x, data$y,
-                          default.units = "native",
-                          hjust = data$hjust, vjust = data$vjust, rot = data$angle,
-                          padding = label.padding, margin = label.margin,
-                          gp = grid::gpar(
-                            col = scales::alpha(data$text.colour %||%
-                                                  data$colour, data$alpha), fontsize = data$size *
-                              .pt, fontfamily = data$family, fontface = data$fontface,
-                            lineheight = data$lineheight
-                          ),
-                          r = label.r
+    default.units = "native",
+    hjust = data$hjust, vjust = data$vjust, rot = data$angle,
+    padding = label.padding, margin = label.margin,
+    gp = grid::gpar(
+      col = scales::alpha(data$text.colour %||%
+        data$colour, data$alpha), fontsize = data$size *
+        .pt, fontfamily = data$family, fontface = data$fontface,
+      lineheight = data$lineheight
+    ),
+    r = label.r
   )
 }
 
-`%||%` <- function (a, b)  {
+`%||%` <- function(a, b) {
   if (!is.null(a)) {
     a
   } else {
